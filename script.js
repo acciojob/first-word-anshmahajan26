cc64e85474ee6fdbb8bfe3b081fa6263173e0d2d
Ansh Mahajan <anshmahajan678@gmail.com>
2026-01-14 11:46:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstWord(s) {
   // your code here
+	let result ="";
+	for(let i =0; i<s.length; i++){
+		if(s[i]==" "){
+			break;
+		}
+		result += s[i];
+	}
+	return result;
 }
 
 // Do not change the code below
